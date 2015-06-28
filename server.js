b563fafe60c84bd4c279503b90a1293c0a5c99bf
alexunwin <alex.unwin@infospace.com>
2015-06-27 20:10:30 -0700

    set socket.io serverr using ec2 host

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var wav = require('wav');
-
-
 var io = require('socket.io')();
+
+
+
+console.log("Listening on port " + 9001)
+io.listen(9001);
+
 io.on('connection', function(socket){
     console.log("Connected");
+    socket.on('data', function(data) {
+        console.log(data);
+    });
 });
-io.listen(9001);
