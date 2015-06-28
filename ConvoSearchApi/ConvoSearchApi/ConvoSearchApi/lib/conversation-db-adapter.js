ef649c95db157fcb3c25e4f97e8a476548eca931
alex nishikawa <alex.d.nishikawa@gmail.com>
2015-06-27 17:30:52 -0700

    simple wrapper around cluster point for conversations

---
+++ ConvoSearchApi/ConvoSearchApi/ConvoSearchApi/lib/conversation-db-adapter.js
@@ -0,0 +1,41 @@
+﻿var cps = require('cps-api');
+
+var conversationDbAdapter = function() {
+     var connection = new cps.Connection('tcp://cloud-us-0.clusterpoint.com:9007', 'First_Test', 'alex.d.nishikawa@gmail.com', 'rfgt&^%ehfkjtrefgF', 'document', 'document/id', { account: 100635 });
+
+    function updateConversation(id, conversation, cb) {
+        if (!id || !conversation)
+            throw new Error("id and words must be valid or exist");
+        
+        var document = {
+            id: id,
+            conversation: conversation
+        };
+             
+        var update_request = new cps.UpdateRequest(document);
+        connection.sendRequest(update_request, cb);
+    }
+
+    function createConversation(id, conversation, cb) {
+        if (!id)
+            throw new Error("id must be valid or exist");
+
+        var document = {
+            id: id
+        };
+        
+        if (conversation)
+            document.conversation = conversation;
+
+        var insert_request = new cps.InsertRequest(document);
+        connection.sendRequest(insert_request, cb);
+    }
+    
+    return {
+        createConversation: createConversation,
+        updateConversation: updateConversation
+    };
+}
+
+
+exports.create = conversationDbAdapter;
--- ConvoSearchApi/ConvoSearchApi/ConvoSearchApi/routes/conversation.js
+++ ConvoSearchApi/ConvoSearchApi/ConvoSearchApi/routes/conversation.js
@@ -1,9 +1,28 @@
 ﻿var express = require('express');
+var moment = require('moment');
+var conversationDbAdapter = require('../lib/conversation-db-adapter');
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+
+    var conn = conversationDbAdapter.create();
+    
+    var id = moment.utc().toDate().toUTCString()
+    var conversation = "test convo"
+
+    conn.createConversation(id, conversation, function (err, insertResponse) {
+        if (err) return console.error(err);
+        
+        conversation = conversation + " more convo";
+
+        conn.updateConversation(id, conversation, function (err, updateResponse) {
+            if (err) return console.error(err);
+            
+            //do whatever next
+        });
+    }); 
 });
 
 module.exports = router;
